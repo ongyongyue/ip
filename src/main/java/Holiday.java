1f39b768702967eaf14cf9a93a2e4ddcf0f2c1f6
ongyongyue <yongyue555@gmail.com>
2026-01-22 16:12:45 +0800

    Removed traces of Duke and added own Chatbot name and intro message

---
+++ src/main/java/Holiday.java
@@ -0,0 +1,11 @@
+public class Holiday {
+    public static void main(String[] args) {
+        String greeting = "---------------------\n"
+                + "Hello! I'm Holiday\n"
+                + "How can I help you?\n"
+                + "---------------------\n"
+                +"Bye! See you again!\n"
+                + "---------------------\n";
+        System.out.println(greeting);
+    }
+}
