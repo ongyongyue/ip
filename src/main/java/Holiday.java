0e3313f3ceef6d1487b364993db5ba7225a8f0aa
ongyongyue <yongyue555@gmail.com>
2026-01-22 16:42:42 +0800

    Made the chat bot echo the message back now

--- src/main/java/Holiday.java
+++ src/main/java/Holiday.java
@@ -1,11 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Holiday {
     public static void main(String[] args) {
-        String greeting = "---------------------\n"
-                + "Hello! I'm Holiday\n"
-                + "How can I help you?\n"
-                + "---------------------\n"
-                +"Bye! See you again!\n"
-                + "---------------------\n";
+
+        String greeting = "\t--------------------------------------------\n"
+                + "\tHello! I'm Holiday\n"
+                + "\tHow can I help you?\n"
+                + "\t--------------------------------------------\n";
         System.out.println(greeting);
+        Scanner scanner  = new Scanner(System.in);
+        String message = " ";
+        boolean isBye = true;
+        while(true){
+            message = scanner.nextLine();
+
+            isBye = message.toLowerCase().equals("bye");
+
+            if(isBye) break;
+
+            System.out.println(
+                    "\t--------------------------------------------\n"
+                    + "\t"
+                    + message
+                    + "\n"
+                    + "\t--------------------------------------------\n"
+            );
+
+        }
+        System.out.println(
+                "\t--------------------------------------------\n"
+                + "\tBye! See you again!\n"
+                + "\t--------------------------------------------\n"
+        );
     }
 }
